5d8dc150349bac82fb563d2502787d8bd23b5d4d
Andrea <ands.sanchez00@gmail.com>
2023-03-29 17:29:51 -0600

    Abort 404 si no lo encuentra

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def home():
         for videojuego in videojuegos:
             if titulo==videojuego['titulo']:
                 videojuegos2.append(videojuego)
+            else:
+                abort(404)
         response={"videojuegos":videojuegos2}
         return render_template('videogame.html', response=response)
         """try:
